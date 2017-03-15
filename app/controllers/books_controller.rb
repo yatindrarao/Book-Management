9620ad346836ed3353604e13f31e16ef6e070829
Yatindra <yatindrarao.rao@gmail.com>
2017-03-15 13:44:48 +0530

    Added Book create functionality with tests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    Book.create(book_params)
+    redirect_to root_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :description, :author)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "books#index"
+  resources :books, only: [:new, :create]
 end
